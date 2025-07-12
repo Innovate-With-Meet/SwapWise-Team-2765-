bd8bdc317011eb8da461887c89b7fc07a369fa99
Meet S Rana. <102943643+Innovate-With-Meet@users.noreply.github.com>
2025-07-12 16:48:32 +0530

    Add files via upload

---
+++ SwapWise-Frontend/FrontEnd/src/CommonPage/Error_Page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export const Error_Page = () => {
+    return (
+        <div className="Error-page">
+            <h1>Error </h1>
+        </div>
+    );
+}
+
+export default Error_Page;
---
+++ SwapWise-Frontend/FrontEnd/src/CommonPage/Landing_Page.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export const LandingPage = () => {
+    return (
+        <>
+            <h1> Home Page
+            </h1>
+        </>
+    )
+
+}
+
+export default LandingPage;
